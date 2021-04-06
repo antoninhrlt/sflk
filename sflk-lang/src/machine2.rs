9a506fd12c2e4af7908659f40cb31114d07c8f2e
anima-libera <anim.libera@gmail.com>
2021-04-06 10:51:55 +0200

    Machine2 on its way

--- sflk-lang/src/machine2.rs
+++ sflk-lang/src/machine2.rs
@@ -5,10 +5,35 @@ use std::collections::HashMap;
 enum BlockOrChain {
 	Block(Block),
 	Chain(Chain),
+	Empty, // TODO: Remove this variant, maybe
 }
 
 struct ExecNode {
 	block_or_chain: BlockOrChain,
 	ip: usize,
 	sub_nodes: Vec<ExecNode>,
+	variables: HashMap<String, Obj>,
+}
+
+impl ExecNode {
+	fn new() -> ExecNode {
+		ExecNode {
+			block_or_chain: BlockOrChain::Empty,
+			ip: 0,
+			sub_nodes: Vec::new(),
+			variables: HashMap::new(),
+		}
+	}
+}
+
+pub struct Mem {
+	exec_tree: ExecNode,
+}
+
+impl Mem {
+	pub fn new() -> Mem {
+		Mem {
+			exec_tree: ExecNode::new(),
+		}
+	}
 }
